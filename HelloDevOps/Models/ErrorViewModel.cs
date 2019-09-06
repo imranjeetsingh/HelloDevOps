5751070ca4770facc4bf6e54780d5c0c2c886278
Ranjeet1 Kumar <Ranjeet1.Kumar@lntinfotech.com>
2019-09-06 11:43:06 +0000

    Commit-First Change

---
+++ HelloDevOps/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HelloDevOps.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
